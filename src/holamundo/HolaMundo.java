b9ec48f2ce9e81fd0bde0a3d59f3a0061496eca7
ruals <ruben@probando.com>
2021-02-20 09:27:15 +0100

    Borro los comentarios desde netbeans

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -11,11 +11,9 @@ package holamundo;
  */
 public class HolaMundo {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic herefdsfsdfs
+
 	// Modificación desde consoloa  GitLocal
 	System.out.println("Hola Mundo");
 	//Fin de la modificación desde consola GitLocal
