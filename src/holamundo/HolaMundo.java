ff19620d3e78e84cd4eff88cbcbff3d83c3813a4
Ruben Alvarez Silva <ruben@probando.com>
2021-02-20 09:38:23 +0100

    Cambio los dos holamundo desde consola

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -10,11 +10,11 @@ public class HolaMundo {
     public static void main(String[] args) {
 
 	// Modificación desde consoloa  GitLocal
-	System.out.println("Hola Mundo");
+	System.out.println("Hola Mundo, como estas?");
 	//Fin de la modificación desde consola GitLocal
         
         //Modificación desde Netbeans
-        System.out.println("Hola Mundo, otra vez");
+        System.out.println("Hola Mundo, otra vez, todo bien");
         //Fin de la modificación desde netbeans
     }
     
